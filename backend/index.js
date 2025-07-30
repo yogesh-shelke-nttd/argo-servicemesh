44823f7ce109b39556871e5ffb8377649cf3a457
yogesh-shelke-nttd <yogeshharichandra.shelke@nttdata.com>
2025-07-30 20:28:15 +0530

    added *

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-app.use(cors({origin: 'http://localhost:8082'} ));
+app.use(cors({origin: "*"} ));
 
 app.get("/api/data", (req, res) => {
     console.log("Received request for data");
